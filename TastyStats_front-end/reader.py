dd529d77716638638a569d4bd29c5b66e2df2f8e
abalaji <abalaji@ncsu.edu>
2018-11-30 16:19:50 -0500

    fixing frontend to recognize feeded image annd produce output

---
+++ TastyStats_front-end/reader.py
@@ -0,0 +1,38 @@
+import csv
+
+# each entry in the csv file
+foods = []
+
+# unique foods in the csv file
+uniqueFoods = {}
+
+def processData():
+    with open('../data/calorie_dataset.csv') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+        for row in csv_reader:
+            tmp = {}
+            if line_count == 0 or len(row[0].strip()) == 0:
+                line_count += 1
+            else:
+                tmp["Food"] = row[0].strip()
+                tmp["FoodSubCategory"] = row[1].strip()
+                tmp["ServingSize"] = row[2].strip()
+                tmp["Fat(g)"] = row[3].strip()
+                tmp["Calories"] = row[4].strip()
+                tmp["Ingredients"] = row[5].strip()
+                foods.append(tmp)
+
+                # add to the option list as well
+                uniqueFoods[row[0].strip()] = 0
+
+def getFoods():
+    processData()
+    foodOptions = []
+    for key, value in uniqueFoods.items():
+        foodOptions.append(key)
+    #print("Found", len(foods), "types of food and", len(foodOptions), "unique foods")
+    return foods, foodOptions
+
+if __name__ == '__main__':
+    processData()
--- TastyStats_front-end/tastystats.py
+++ TastyStats_front-end/tastystats.py
@@ -1,5 +1,10 @@
 from flask import Flask, render_template, url_for
 from forms import getFoodForm
+from reader import getFoods
+
+# Global Variables, don'r instantiate reader again
+foods = []
+uniqueFoods = []
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'da50f45e806d34c2f51f4baf9c1f7350'
@@ -52,16 +57,41 @@ def about():
 @app.route("/explore", methods=['GET','POST'])
 @app.route("/getStats", methods=['GET','POST'])
 def getStats():
+    print("Yes, listening")
     form = getFoodForm()
     pred = {}
     act = {}
+    your_input = ""
+    allFoods, uniqueFoods = getFoods()
+    print("Validating form...")
     if form.validate_on_submit():
-        act = foods[1]
+        print(form.food.data)
+        act = getSimilarFoods(form.food.data)
+        your_input = form.image.data
+        print("We found", len(act), "actual foods")
+        # get prediction
+        # string output from our model get a string output
+        #pred = {}
+        pred = "Burger" # hardcoding temporarily
+        pred = getSimilarFoods(pred) #get all related entries from our csv
+        print("and, we found", len(pred), "matching predictions")
 
-        #get prediction
-        pred = foods[0]
-    return render_template('getStats.html', form=form, pred=pred, actual=act, foods=foods[2:], title="Ok, let's crunch some numbers.")
+    return render_template('getStats.html', form=form, input=your_input,
+                           pred=pred, actual=act, foods=allFoods,
+                           title="Ok, let's crunch the food...")
+
+
+def getSimilarFoods(name):
+    simFoods = []
+    for f in foods:
+        if f['Food'] == name:
+            simFoods.append(f)
+
+    return simFoods
 
 
 if __name__ == '__main__':
+    if len(foods) == 0:
+        foods, uniqueFoods = getFoods()
+    print("wassup")
     app.run(debug=True)
